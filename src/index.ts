4449a427fcf3679f6dd871f7b1546f88f94b9aa3
lynx <david.frnoch@creativehill.cz>
2022-01-15 18:18:26 +0100

    env

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-console.log('Hello World');
+import express, {Application} from "express";
+import dotevn from "dotenv";
 
-console.log('Hello World');
+
+dotevn.config();
+const app: Application = express();
+
+
+
+
+
+
+app.listen(process.env.port, () => {
+    console.log('Server is running on port ' + process.env.port);
+});
