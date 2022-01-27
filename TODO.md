ee7491545cecbc0f74a2867e20a147a43080f22d
lynx <david.frnoch@creativehill.cz>
2022-01-27 23:10:02 +0100

    client: init service & contexts

---
+++ TODO.md
@@ -0,0 +1,1 @@
+1. RefreshToken > all
--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,6 +1,4 @@
 import axios from "axios";
-// import { signOut } from "../contexts/auth.context";
-
 
 function setupAPIClient(ctx: any = undefined) {
     const token = localStorage.getItem("token");
