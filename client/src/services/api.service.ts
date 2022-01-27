9b97e2108ee98d0c729da03b3e3085f9db123ecf
lynx <david.frnoch@creativehill.cz>
2022-01-27 23:05:18 +0100

    client: init service & contexts

---
+++ client/src/services/api.service.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+// import { signOut } from "../contexts/auth.context";
+
+
+function setupAPIClient(ctx: any = undefined) {
+    const token = localStorage.getItem("token");
+    const api = axios.create({
+        baseURL: process.env.API_URL,
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Bearer ${token}`,
+        },
+    });
+
+    return api;
+}
+
+export const api = setupAPIClient();
