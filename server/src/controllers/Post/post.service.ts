93f72a7fe285fd6b692d0614d879a8dca65b3420
lnx <in@lnx.rip>
2022-02-01 10:35:24 +0100

    server: create post service

--- server/src/controllers/Post/post.service.ts
+++ server/src/controllers/Post/post.service.ts
@@ -1,1 +1,14 @@
-//todo
+import { Model } from "mongoose";
+
+
+class PostService{ 
+    // private model: Model<any>; make it better
+
+
+    public getLikes(model: Model<any>){
+        return model.find();
+    }
+
+
+
+}
